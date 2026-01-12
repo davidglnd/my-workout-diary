02b5b37a33395f90beb2e16eb44f7948fbf22811
davidglnd <david.galindo.dgp@gmail.com>
2026-01-12 13:27:46 +0100

    feat(form): collect and validate form data

--- src/js/pages/new-routine.js
+++ src/js/pages/new-routine.js
@@ -1,41 +1,74 @@
 
-import { validateRoutineForm } from '../validators/new-routine-form-validator.js';
+import { validateRoutineDetailsForm, validateRoutineDaysForm } from '../validators/new-routine-form-validator.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     const buttonNextStep = document.getElementById('next-step');
+    const buttonSecondStep = document.getElementById('second-step');
 
-    buttonNextStep.addEventListener('click', clickedButton);
+    buttonNextStep.addEventListener('click', clickedNextStep);
+    buttonSecondStep.addEventListener('click', clickedSecondStep);
 })
 
-function clickedButton() {
+function clickedNextStep() {
     const formDetails = document.querySelector('#routine-details');
     const formDetailsInputs = new FormData(formDetails);
     const formDetailsObject = Object.fromEntries(formDetailsInputs);
 
     formDetailsObject.workout_days = Number(formDetailsObject.workout_days);
 
-    let validateformDetailsObject = validateRoutineForm(formDetailsObject);
+    let validateformDetails = validateRoutineDetailsForm(formDetailsObject);
 
-    if(typeof validateformDetailsObject === 'string'){
+    if(typeof validateformDetails === 'string'){
         const error = document.querySelector('#error-msg');
-        error.textContent = validateformDetailsObject;
+        error.textContent = validateformDetails;
         error.classList.add('active');
         setTimeout(() => {
             error.classList.remove('active');
         }, 3000)
 
     }else{
-        nextStep(formDetailsObject);
+        showFormDays();
+        clearLocalStorage();
+        saveToLocalStorage(validateformDetails);
     }
 }
 
-function nextStep(formDetailsObject){
-    console.log(formDetailsObject);
+function showFormDays(){
     const formDays = document.querySelector('#routine-days');
     const formDetails = document.querySelector('#routine-details');
 
     formDetails.classList.remove('active');
     formDays.classList.add('active');
-
-    
 };
+
+function clickedSecondStep(){
+    const routineDetails = JSON.parse(localStorage.getItem('routine'));
+        
+    const checkedDays = Array.from(
+        document.querySelectorAll('input[type="checkbox"]:checked')
+    ).map((checkbox) => checkbox.value);
+
+    let validateformDays = validateRoutineDaysForm(checkedDays, routineDetails[2]);
+
+    if(typeof validateformDaysObject === 'string'){
+        const error = document.querySelector('#error-days');
+        error.textContent = validateformDays;
+        error.classList.add('active');
+        setTimeout(() => {
+            error.classList.remove('active');
+        }, 3000)
+    }else{
+        //Aqui ya tendriamos que pushear los dias a la base de datos
+        console.log(routineDetails, validateformDays);
+        // son dos arrays
+    }
+    
+}
+
+function saveToLocalStorage(validateformDetailsObject){
+    localStorage.setItem('routine', JSON.stringify(validateformDetailsObject));
+}
+
+function clearLocalStorage(){
+    localStorage.removeItem('routine');
+}
--- src/js/validators/new-routine-form-validator.js
+++ src/js/validators/new-routine-form-validator.js
@@ -1,12 +1,25 @@
-export function validateRoutineForm(formDetails){
-    const numberDays = [];
+export function validateRoutineDetailsForm(formDetails){
+    const arrayRoutineDetails = [];
     let error = '';
     if (formDetails.name === '' || formDetails.description === ''){
         return error = 'Por favor, complete todos los campos.';
     }else if (formDetails.workout_days > 7 || formDetails.workout_days < 2){
         return error = 'Por favor, seleccione un numero de dias de entrenamiento entre 2 y 7.';
     }else if (error === ''){
-        numberDays.push(formDetails.workout_days);
-        return numberDays;
+        arrayRoutineDetails.push(formDetails.name);
+        arrayRoutineDetails.push(formDetails.description);
+        arrayRoutineDetails.push(formDetails.workout_days);
+        return arrayRoutineDetails;
+    }
+}
+
+export function validateRoutineDaysForm(checkedDays, workoutDays){
+    const arrayRoutineDays = [];
+    let error = '';
+    if (checkedDays.length < workoutDays || checkedDays.length > workoutDays){
+        return error = 'Por favor, seleccione ' + workoutDays + ' dias de entrenamiento.';
+    }else if (error === ''){
+        arrayRoutineDays.push(checkedDays);
+        return arrayRoutineDays;
     }
 }
